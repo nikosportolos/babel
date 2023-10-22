d8440cf0fbd89157ac9b078614bb4f56847ecb74
Nikos Portolos <nikos.portolos@gmail.com>
2023-10-23 01:57:42 +0300

    #27 Add --dry-run flag argument on clean unused-translations command

--- lib/src/exceptions/exceptions.dart
+++ lib/src/exceptions/exceptions.dart
@@ -1,5 +1,17 @@
+import 'package:ansix/ansix.dart';
 import 'package:dart_cmder/dart_cmder.dart';
 
 part 'localization_options.dart';
 
 class BabelException implements Exception {}
+
+class UnusedTranslationsFound implements Exception {
+  const UnusedTranslationsFound(this.count);
+
+  final int count;
+
+  @override
+  String toString() {
+    return 'Babel found ${count.toString().bold()} unused translations.';
+  }
+}
